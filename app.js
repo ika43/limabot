46dec80f6bd08a061b1f9d3c6e1ab46f44243b9e
ika43 <ivanlaba92@gmail.com>
2018-10-25 20:50:07 +0200

    webhook

--- app.js
+++ app.js
@@ -19,5 +19,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// handling get route for verification
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === process.env.VALIDATION_TOKEN) {
+      console.log("Validating webhook");
+      res.status(200).send(req.query['hub.challenge']);
+  } else {
+      console.error("Failed validation. Make sure the validation tokens match.");
+      res.sendStatus(403);
+  }
+});
+
 
 module.exports = app;
